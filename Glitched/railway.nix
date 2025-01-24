ff9f8ce6c9617928afb3d49144d2ce5ff77fea5f
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2025-01-24 16:51:10 +0600

    deploy railway version fixed

--- Glitched/railway.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8ac1c1c.tar.gz") { } }:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.php80
-    pkgs.composer
-    pkgs.nodejs
-    pkgs.mysql
-  ];
-}
