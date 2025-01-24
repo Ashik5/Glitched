036c176e476d9a2f682706ecb8565603105d00c8
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2025-01-24 17:09:35 +0600

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BlogsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth')->group(function () {
+    Route::post('/user/update-profile', [UserController::class, 'updateProfile']);
+});
+// Route::post('/user/update-profile', [UserController::class, 'updateProfile']);
+
+Route::middleware('auth')->group(function () {
+    Route::put('/blogs/{id}', [BlogsController::class, 'updateBlog']);
+});
+
+Route::post('/blogs', [BlogsController::class, 'createBlog']); 
