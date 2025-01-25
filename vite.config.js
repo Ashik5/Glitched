3588a208e938361fefb7dee80e7ed788754fe873
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2025-01-25 22:08:40 +0600

    config

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig(({ command }) => {
     const isProduction = command === 'build' || process.env.APP_ENV === 'production';
     return {
+        base: isProduction ? 'https://glitched.onrender.com/' : '/',
         plugins: [
             laravel({
                 input: ['resources/js/app.jsx'],
                 refresh: !isProduction,
+                buildDirectory: 'build',
             }),
             react(),
         ],
-        resolve: {
-            alias: {
-                '@assets': path.resolve(__dirname, 'resources/assets'),
-            },
-        },
         server: {
+            https: true,
             host: '0.0.0.0',
             port: 5173,
-            https: true,
             strictPort: true,
             hmr: {
                 host: 'glitched.onrender.com',
             },
             cors: {
-                origin: ['https://glitched.onrender.com', 'http://glitched.onrender.com'],
+                origin: 'https://glitched.onrender.com',
                 methods: ['GET', 'POST', 'OPTIONS'],
-                allowedHeaders: ['Content-Type', 'Authorization'],
             },
         },
         build: {
